fe1262dcc418db6d3d9f0fcbdafb3e99d569412d
hyeon <ish408501@gmail.com>
2023-08-25 00:26:58 +0900

    누적합 문제입니다

--- baekjoon/2559.py
+++ baekjoon/2559.py
@@ -1,1 +1,12 @@
-print("Hello python")
+n , k = map(int,input().split())
+arr = list(map(int,input().split()))
+for i in range(1,len(arr)):
+    arr[i]=arr[i]+arr[i-1]
+
+ans=arr[k-1]  # 제일 처음의 연속된 며칠의 합을 미리 넣음
+for i in range(k,len(arr)):
+    check=arr[i]-arr[i-k]
+    ans=max(ans,check)
+        
+
+print(ans)
